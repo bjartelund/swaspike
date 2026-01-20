2fcaa766234716c803732dc00f1c05b2e405cf43
Bjarte Aarmo Lund <bjarte.lund@bouvet.no>
2026-01-20 08:48:02 +0100

    make compilable typescript out of starter template

--- pages/project/[path].js
+++ pages/project/[path].js
@@ -86,9 +86,9 @@ export async function getStaticProps({ params }) {
   const project = projects.find((proj) => proj.slug === params.path);
   const res = await fetch(`https://api.github.com/repos/${project.path}`);
   const data = await res.json();
-  project.open_issues = data.open_issues;
-  project.subscribers_count = data.subscribers_count;
-  project.stargazers_count = data.stargazers_count;
+  project.open_issues = data.open_issues ?? null;
+  project.subscribers_count = data.subscribers_count ?? null;
+  project.stargazers_count = data.stargazers_count ?? null;
   return { props: { project } };
 }
 
